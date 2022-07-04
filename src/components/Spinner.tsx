eb281b3d2adf327f32f2869c30c8c470c0bdc175
carter-mathieu <cartermathieu96@gmail.com>
2022-07-04 19:27:34 -0400

    update remote

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -3,7 +3,7 @@ import spinny_cat from "../assets/spinny_cat.gif";
 
 const Spinner = () => {
 	return (
-		<div className="w-100 mt-20">
+		<div className="w-100 mt-20 ml-20">
 			<img className="" width={180} alt="Spinny Cat" src={spinny_cat} />
 		</div>
 	);
